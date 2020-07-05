c2178a7f7acf22beb4e8183bdcc48aa4ada6f250
ankitmishra23 <ankit23m071996@gmail.com>
2020-07-05 14:19:42 +0530

    hello

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component,OnInit} from '@angular/core';
+import {FormBuilder,FormControl,FormGroup,FormArray, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+class loginuser{
+  name:string;
+  email:string;
+  dob:Date;
+  country:string;
+  gender:string;
+  status:string;
+  street:string;
+  door:string;
+  pin:string;
+
+}
+
+export class AppComponent implements OnInit {
+  title = 'app';
+  
+  submitform=false;
+  login:FormGroup;
+  
+data=loginuser;
+adddata=[];
+  constructor(private log:FormBuilder)
+  { this.ngOnInit(); }
+  ngOnInit()
+  {
+    this.login=this.log.group({
+      name:this.log.control("",Validators.required),
+      email:this.log.control("",[Validators.email,Validators.required]),
+      dob:this.log.control("",Validators.required),
+      country:this.log.control("",Validators.required),
+      gender:this.log.control(""),
+      status:this.log.control(""),
+      street:this.log.control("",Validators.required),
+      door:this.log.control("",Validators.required),
+      pin:this.log.control("",[Validators.required,Validators.maxLength(6),Validators.minLength(6)])
+    })
+  }
+  
+getdata()
+{
+  return this.login.controls;
+  //console.log(this.login.value);
+}
+  onSubmit()
+  {
+    this.submitform=true;
+    this.data=this.login.value;
+    this.adddata.push(this.data);
+    }
+
+}
